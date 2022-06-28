2a59be9a7cdd61addacae4af85867525b0a459de
MJ <mikey.j.1228@gmail.com>
2022-06-28 15:16:58 +0900

    Add button for choosing dimensions. Also played around with grid to figure out a dynamic solution

--- js-code.js
+++ js-code.js
@@ -1,15 +1,19 @@
 const container = document.createElement('div');
 container.classList.add('container');
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 5; i++) {
     let square = document.createElement('div');
     container.appendChild(square);
     square.classList.add('row');
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < 5; j++) {
         let col = document.createElement('div');
         square.appendChild(col);
         col.classList.add('col');
     }
 };
+
+const button = document.createElement('button');
+button.classList.add('dimensions');
+button.innerText = 'Dimensions';
 
 function highlight() {
     this.style.backgroundColor = 'yellow';
@@ -17,6 +21,7 @@ function highlight() {
 
 
 const bod = document.querySelector('body');
+bod.appendChild(button);
 bod.appendChild(container);
 
 const grids = document.querySelectorAll('.row, .col');
