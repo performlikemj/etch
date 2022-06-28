50873f81fda0a075a274d8af598cd1b08c547557
MJ <mikey.j.1228@gmail.com>
2022-06-28 15:45:00 +0900

    Add functionality for highlight after dimension change. Albeit code does not follow 'Don't Repeat Yourself' DRY

--- js-code.js
+++ js-code.js
@@ -1,28 +1,55 @@
 const container = document.createElement('div');
 container.classList.add('container');
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 16; i++) {
     let square = document.createElement('div');
     container.appendChild(square);
     square.classList.add('row');
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 16; j++) {
         let col = document.createElement('div');
         square.appendChild(col);
         col.classList.add('col');
     }
 };
 
+/* Grid dimension functionality */
+
 const button = document.createElement('button');
 button.classList.add('dimensions');
 button.innerText = 'Dimensions';
 
+let size = 16;
+button.addEventListener('click', () => {
+    let dimension = prompt('What size do you want the grid to be (default 16x16)', size);
+    size = dimension;
+    const boxes = document.querySelectorAll('.row, .col');
+    boxes.forEach(box => box.remove());
+    for (let i = 0; i < size; i++) {
+        let square = document.createElement('div');
+        container.appendChild(square);
+        square.classList.add('row');
+        for (let j = 0; j < size; j++) {
+            let col = document.createElement('div');
+            square.appendChild(col);
+            col.classList.add('col');
+        }
+    };
+    let grids = document.querySelectorAll('.row, .col');
+    grids.forEach(grid => grid.addEventListener('mouseover', highlight));
+});
+
+
+/* Highlight functionality */
+
 function highlight() {
     this.style.backgroundColor = 'yellow';
 }
+
+/* Highlight functionality end */
 
 
 const bod = document.querySelector('body');
 bod.appendChild(button);
 bod.appendChild(container);
 
-const grids = document.querySelectorAll('.row, .col');
+let grids = document.querySelectorAll('.row, .col');
 grids.forEach(grid => grid.addEventListener('mouseover', highlight));
