1433731960b0cecbf1fce4a22ccc2a1b73ffd5fd
MJ <mikey.j.1228@gmail.com>
2022-06-28 16:02:40 +0900

    Add random color pencil functionality

--- js-code.js
+++ js-code.js
@@ -19,29 +19,37 @@ button.innerText = 'Dimensions';
 
 let size = 16;
 button.addEventListener('click', () => {
-    let dimension = prompt('What size do you want the grid to be (default 16x16)', size);
-    size = dimension;
-    const boxes = document.querySelectorAll('.row, .col');
-    boxes.forEach(box => box.remove());
-    for (let i = 0; i < size; i++) {
-        let square = document.createElement('div');
-        container.appendChild(square);
-        square.classList.add('row');
-        for (let j = 0; j < size; j++) {
-            let col = document.createElement('div');
-            square.appendChild(col);
-            col.classList.add('col');
-        }
-    };
-    let grids = document.querySelectorAll('.row, .col');
-    grids.forEach(grid => grid.addEventListener('mouseover', highlight));
+    let dimension = prompt('What size do you want the grid \
+                            to be (limit 100)', size);
+    if (dimension > 100) {
+        dimension = prompt('Size limit of 100. Please choose again', size);
+    } else {
+        size = dimension;
+        const boxes = document.querySelectorAll('.row, .col');
+        boxes.forEach(box => box.remove());
+        for (let i = 0; i < size; i++) {
+            let square = document.createElement('div');
+            container.appendChild(square);
+            square.classList.add('row');
+            for (let j = 0; j < size; j++) {
+                let col = document.createElement('div');
+                square.appendChild(col);
+                col.classList.add('col');
+            }
+        };
+        let grids = document.querySelectorAll('.row, .col');
+        grids.forEach(grid => grid.addEventListener('mouseover', highlight));
+    }
 });
 
 
 /* Highlight functionality */
 
 function highlight() {
-    this.style.backgroundColor = 'yellow';
+    let x = Math.floor((Math.random() * 255) + 1);
+    let y = Math.floor((Math.random() * 255) + 1);
+    let z = Math.floor((Math.random() * 255) + 1);
+    this.style.backgroundColor = `rgb(${x},${y},${z})`;
 }
 
 /* Highlight functionality end */
